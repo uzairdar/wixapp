68e06196fc9f2c3bb84cb6dc6083cb31eca1f42b
uzair dar <uzairdar01@gmail.com>
2021-01-25 21:39:34 +0500

    add section added

--- src/Components/AddButton.js
+++ src/Components/AddButton.js
@@ -1,21 +1,43 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Button } from "reactstrap";
-function AddButton() {
+import { setCompleted } from "../redux/ActionCreators";
+import Header from "./Header";
+function AddButton(props) {
+  const { index, tasks, setCompleteData, setSelected, selected } = props;
+
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>
-      <Button
-        // className="btns-color"
-        style={{
-          marginBottom: "-3vh",
-          zIndex: "2",
-          borderRadius: "50%  50%",
-        }}
-        color="primary"
-      >
-        +
-      </Button>
+      {selected !== index && (
+        <Button
+          // className="btns-color"
+          style={{
+            marginBottom: "-3vh",
+            zIndex: "2",
+            borderRadius: "50%  50%",
+          }}
+          color="primary"
+          title="Add Section Here"
+          onClick={() => setSelected(index)}
+        >
+          +
+        </Button>
+      )}
     </div>
   );
 }
-
-export default AddButton;
+const mapStateToProps = (state) => {
+  console.log("state", state.User);
+  // const { user } = state.state;
+  // const {tasks}=
+  const { User } = state;
+  const heading = User.data.Heading;
+  const tasks = User.completedTasks;
+  return { User, tasks };
+};
+const mapDispatchtoProps = (dispatch) => {
+  return {
+    setCompleteData: (data) => dispatch(setCompleted(data)),
+  };
+};
+export default connect(mapStateToProps, mapDispatchtoProps)(AddButton);
